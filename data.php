79c73142716bad7ef85a39e911185c2684cd2142
nisol91 <nicolasolzi91@gmail.com>
2019-01-27 19:02:33 +0100

    visti 3 metodi

--- data.php
+++ data.php
@@ -7,5 +7,7 @@ $data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
 $dataset = json_encode($data);
 
 
+//scommentato in caso di chiamata ajax da js.
+echo $dataset;
 
 ?>
--- script.js
+++ script.js
@@ -2,29 +2,37 @@
 $(document).ready(function() {
 
 
+  $.ajax({
+    url: 'http://localhost/advanced_charts/data.php',
+    method: 'GET',
+    success: function (data) {
+      var my_data = JSON.parse(data);
+      console.log(my_data);
+      //grafico
+      var mesi = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+      var ctx = $('.line-chart');
+      var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
 
-
-
-
-  //grafico
-  var mesi = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  var ctx = $('.line-chart');
-  var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'line',
-
-      // The data for our dataset
-      data: {
+        // The data for our dataset
+        data: {
           labels: mesi,
           datasets: [{
-              label: "My First dataset",
-              backgroundColor: 'rgb(180, 139, 119)',
-              borderColor: 'rgb(53, 72, 154)',
-              data: ,
+            label: "My First dataset",
+            backgroundColor: 'rgb(180, 139, 119)',
+            borderColor: 'rgb(53, 72, 154)',
+            data: my_data,
           }]
-      },
+        },
 
-      // Configuration options go here
-      options: {}
+        // Configuration options go here
+        options: {}
+      });
+    },
   });
+
+
+
+
 });
